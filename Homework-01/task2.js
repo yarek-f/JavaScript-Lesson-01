33ccf109886d72aa1d4bb0329bfafeb91c1b38aa
LogosItAcademy <Tester99037@gmail.com>
2020-06-05 11:47:44 +0300

    fixed

--- Homework-01/task2.js
+++ Homework-01/task2.js
@@ -5,16 +5,15 @@ let password = 'TheMaster';
 
 if (login == 'Admin') {
 	console.log('Please enter your password');
+	if (password == 'TheMaster') {
+		console.log('Welcome!');
+	} else if (password !== 'TheMaster') {
+		console.log("Wrong password")
+	} else {
+		comsol.log('Canceled')
+	}
 } else if (login !== 'Admin') {
 	console.log("I don't know you");
-} else {
-	comsol.log('Canceled')
-}
-
-if (password == 'TheMaster') {
-	console.log('Welcome!');
-} else if (password !== 'TheMaster') {
-	console.log('Wrong password')
 } else {
 	comsol.log('Canceled')
 }
